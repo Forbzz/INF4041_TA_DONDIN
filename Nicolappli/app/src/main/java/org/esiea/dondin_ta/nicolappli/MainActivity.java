5a09a6d6ed1f017a08b4715f339ed85762a26228
Forbzz <ta@et.esiea.fr>
2015-10-12 15:23:49 +0200

    not even the final flash

--- Nicolappli/app/src/main/java/org/esiea/dondin_ta/nicolappli/MainActivity.java
+++ Nicolappli/app/src/main/java/org/esiea/dondin_ta/nicolappli/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -15,16 +16,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+
+
     public void gayMode(View v) {
 
         Toast.makeText(getApplicationContext(), getString(R.string.gaymode), Toast.LENGTH_LONG).show();
         RelativeLayout bgElement = (RelativeLayout) findViewById(R.id.container);
-       int switchgay =0;
 
-                bgElement.setBackgroundColor(Color.argb(255, 255, 0, 127));
 
-            }
+        bgElement.setBackgroundColor(Color.argb(255, 255, 0, 127));
+
+    }
+
+    public void gayModeDesactivated(View v) {
+
+        Toast.makeText(getApplicationContext(), getString(R.string.gaymodedesact), Toast.LENGTH_LONG).show();
+        RelativeLayout bgElement = (RelativeLayout) findViewById(R.id.container);
+
+
+        bgElement.setBackgroundColor(Color.WHITE);
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        RelativeLayout bgElement = (RelativeLayout) findViewById(R.id.container);
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.homo:
+                if (checked)
+                    // homo
+                    bgElement.setBackground(getResources().getDrawable(R.drawable.homo));
+                    break;
+            case R.id.lesbia:
+                if (checked)
+                    // lesbia
+                    bgElement.setBackground( getResources().getDrawable(R.drawable.lesbi) );
+                    break;
+
+            case R.id.animal:
+                if (checked)
+                    // animal
+                bgElement.setBackground(getResources().getDrawable(R.drawable.animal));
+                    break;
+        }
+    }
+
+
 
         }
-       
+
 
